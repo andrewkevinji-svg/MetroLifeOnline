90ca84c3fefc2112b5fa96292d209a1065943d8f
andrewkevinji-svg <andrewkevinji@gmail.com>
2026-05-03 11:40:08 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "",
+        name: "Metro Life Online",
         // Activity type number (0 = Playing).
         type: 4, 
       },
